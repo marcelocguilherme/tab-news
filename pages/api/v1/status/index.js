13631ea522cc32c38211bbf3a88404ad928ab7b4
Marcelo Guilherme <marcelocguilherme@gmail.com>
2025-05-30 01:40:10 +0000

    adiciona database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+    const result = await database.query("select 1 + 1 as soma2;");
+    console.log(result.rows);
     response.status(200).json({ message: "muito bom, vamos avançando" });
 }
 
